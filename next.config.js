b1c19f0636877e87a7fe7f94567bd7e416176abe
Kamilah Mohchin <kamilahmohchin@gmail.com>
2023-01-05 14:50:53 +0000

    First working version: user can input text prompt and generate an image.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  optimizeFonts: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'replicate.delivery',
+      },
+      {
+        protocol: 'https',
+        hostname: 'replicate.delivery',
+      },
+    ],
+    minimumCacheTTL: 1500000,
+  },
 }
-
 module.exports = nextConfig
